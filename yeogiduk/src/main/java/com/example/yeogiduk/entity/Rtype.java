b1b4987bc77196f5c0b4867ac39886fc52d1a84c
parkyunsir <qdt1030@naver.com>
2023-11-08 18:37:52 +0900

    rtype

--- yeogiduk/src/main/java/com/example/yeogiduk/entity/Rtype.java
+++ yeogiduk/src/main/java/com/example/yeogiduk/entity/Rtype.java
@@ -1,4 +1,14 @@
 package com.example.yeogiduk.entity;
 
+import lombok.Builder;
+
 public class Rtype {
+    private String rType;
+//    private int rType;
+
+    @Builder
+    public Rtype(String rType){
+        this.rType=rType;
+    }
+
 }
