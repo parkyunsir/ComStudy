a57be3e4d22669d0a22609b612713eb07ad66c2f
parkyunsir <qdt1030@naver.com>
2023-11-11 00:05:32 +0900

    Menu 수정

--- yeogiduk/src/main/java/com/example/yeogiduk/dto/MenuDto.java
+++ yeogiduk/src/main/java/com/example/yeogiduk/dto/MenuDto.java
@@ -3,20 +3,25 @@ package com.example.yeogiduk.dto;
 import com.example.yeogiduk.entity.Menu;
 import com.example.yeogiduk.entity.Rtype;
 import lombok.Builder;
+import lombok.Data;
+import lombok.Getter;
 
+@Getter
+@Data
 public class MenuDto {
-    private Integer Rstid;
+    private Long rstId;
     private String menu;
     private Integer price;
 
-    @Builder
-    public MenuDto(Integer Rstid, String menu, Integer price){
-        this.Rstid=Rstid;
-        this.menu=menu;
-        this.price=price;
+    public Menu toEntity() {
+        return new Menu(rstId, menu, price);
     }
 
-    public Menu toEntity() {
-        return new Menu(Rstid, menu, price);
+    public Long getRstId(){
+        return rstId;
+    }
+
+    public void setRstId(Long rstId){
+        this.rstId=rstId;
     }
 }
