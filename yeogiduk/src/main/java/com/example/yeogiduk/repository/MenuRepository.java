de3601234c59ea4d1c202efc5899df3cf1ee3d63
psy-er <89902211+psy-er@users.noreply.github.com>
2023-11-11 01:11:40 +0900

    띄어쓰기?

--- yeogiduk/src/main/java/com/example/yeogiduk/repository/MenuRepository.java
+++ yeogiduk/src/main/java/com/example/yeogiduk/repository/MenuRepository.java
@@ -4,7 +4,6 @@ import com.example.yeogiduk.dto.MenuDto;
 import com.example.yeogiduk.entity.Menu;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-
 import java.util.List;
 
 public interface MenuRepository extends JpaRepository<Menu, Long> {
