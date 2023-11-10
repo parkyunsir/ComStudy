9fddacb9ef7f8192f385dfa051018c103cdef92a
takealook2 <a1vm5h@gmail.com>
2023-11-10 22:43:04 +0900

    restuarant-수정중

--- yeogiduk/src/main/java/com/example/yeogiduk/dto/StudentDto.java
+++ yeogiduk/src/main/java/com/example/yeogiduk/dto/StudentDto.java
@@ -1,4 +1,21 @@
 package com.example.yeogiduk.dto;
 
+import com.example.yeogiduk.entity.Restaurant;
+import com.example.yeogiduk.entity.Student;
+import lombok.*;
+
+import java.util.List;
+
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
 public class StudentDto {
+    private String sEmail;
+    private String password;
+    private List<Long> likes;
+    private String token;
+
+    public Student toEntity() { return new Student(sEmail, password, likes); }
 }
--- yeogiduk/src/main/java/com/example/yeogiduk/service/RtypeService.java
+++ yeogiduk/src/main/java/com/example/yeogiduk/service/RtypeService.java
@@ -1,4 +1,16 @@
 package com.example.yeogiduk.service;
 
-public class RtypeService {
+import com.example.yeogiduk.dto.RtypeDto;
+import com.example.yeogiduk.entity.Rtype;
+import com.example.yeogiduk.repository.RtypeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+
+public class RtypeService{
+    @Autowired
+    private RtypeRepository rtypeRepository;
+
+    public Rtype rupload(RtypeDto rtypeDto){
+        Rtype rtype = rtypeRepository.findBytype(rtypeDto.toString());
+        return rtype;
+    }
 }
