ab3bf07c69e77f1e0bc35c0c10fe848417237706
psy-er <89902211+psy-er@users.noreply.github.com>
2023-11-22 14:22:26 +0900

    이미지 파일 백엔드 구현 실패 리액트로 하겠습니다

--- yeogiduk/src/main/java/com/example/yeogiduk/dto/ReviewDto.java
+++ yeogiduk/src/main/java/com/example/yeogiduk/dto/ReviewDto.java
@@ -1,17 +1,12 @@
 package com.example.yeogiduk.dto;
-
-import com.example.yeogiduk.entity.Image;
 import com.example.yeogiduk.entity.Review;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.ToString;
+import lombok.*;
 import lombok.extern.slf4j.Slf4j;
 
-import java.text.SimpleDateFormat;
-import java.time.LocalTime;
-import java.time.ZoneId;
-import java.time.format.DateTimeFormatter;
 import java.util.Date;
 
 import static java.time.LocalTime.now;
@@ -28,21 +23,44 @@ public class ReviewDto {
     private String content;
     private Date date;
     private int star;
-    //private LongBlob image;
+    //private String Email;
+    //private Long id;
+    //private String originFileName;
+    //private String fullPath;
+/*
+    public File toEntity() {
+        return File.builder()
+                .id(this.id)
+                .originFileName(this.originFileName)
+                .fullPath(this.fullPath)
+                .build();
+    }
+    @Builder
+    public void ImageDto(Long id, String originFileName, String fullPath) {
+        this.id = id;
+        this.originFileName = originFileName;
+        this.fullPath = fullPath;
+    }
+
+ */
     public static ReviewDto createReviewDto(Review review) {
         return new ReviewDto(
                 review.getViewId(),
                 review.getRstId(),
                 review.getEmail(),
-                review.getContent(), // 머지 후 에러 안날듯!
+                review.getContent(),
                 review.getDate(),
+                //review.getRestaurant().getRstId(), // 머지 후 에러 안날듯!
                 review.getStar()
+                //review.getEmail(),
+                //review.getId(),
+                //review.getOriginFileName(),
+                //review.getFullPath()
+
         );
     }
-
     public Date setNow() {
         Date now = new Date();
         return now;
     }
-
 }
--- yeogiduk/src/main/java/com/example/yeogiduk/repository/ImageRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.yeogiduk.repository;
-
-import com.example.yeogiduk.entity.Image;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ImageRepository extends JpaRepository<Image, Long> {
-}
