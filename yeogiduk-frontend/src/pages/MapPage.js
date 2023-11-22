a313f8d1d1916573232d054bc958d74230b18f8f
takealook2 <a1vm5h@gmail.com>
2023-11-23 05:45:33 +0900

    map 추가하고 수정중

---
+++ yeogiduk-frontend/src/pages/MapPage.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import HeaderContainer from '../containers/common/HeaderContainer';
+import MapContainer from '../containers/map/MapContainer';
+
+const Map = () => {
+  return (
+    <>
+        <HeaderContainer />
+        <MapContainer />
+    </>
+  );
+};
+
+export default Map;
