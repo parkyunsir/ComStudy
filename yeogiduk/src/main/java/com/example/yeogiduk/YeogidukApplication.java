ef0973d146968bf7e39c25d464e3068553cc895a
psy-er <89902211+psy-er@users.noreply.github.com>
2023-11-24 20:08:31 +0900

    front

--- yeogiduk/src/main/java/com/example/yeogiduk/YeogidukApplication.java
+++ yeogiduk/src/main/java/com/example/yeogiduk/YeogidukApplication.java
@@ -7,9 +7,7 @@ import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
 
-@SpringBootApplication(exclude = { SecurityAutoConfiguration.class })
-@EnableAutoConfiguration(exclude={DataSourceAutoConfiguration.class})
-//@ComponentScan(basePackages = {"com.example.yeogiduk.repository.MenuRepository"})
+@SpringBootApplication
 public class YeogidukApplication {
 	public static void main(String[] args){
 		SpringApplication.run(YeogidukApplication.class, args);
